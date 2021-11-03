ae457de7965ebdbb48804f36cd3186839f93d5c8
Валентин <valmak04@gmail.com>
2021-11-03 22:41:55 +0400

    Небольшие изменения: Добавил перетосовку. Метод makeDeck стал private. Создание колоды переехало в конструктор.

--- src/src/CardPAc/Deck.java
+++ src/src/CardPAc/Deck.java
@@ -1,18 +1,23 @@
 package CardPAc;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Deck {
     private Card[] deck;
+    Random random = new Random();
 
+    //Конструктор
     public Deck(int size) {
         if (size != 36 & size != 52) {
             throw new ArrayIndexOutOfBoundsException("Такой колоды не существует!");
         }
         this.deck = new Card[size];
+        makeDeck();
     }
 
-    public void makeDeck() {
+    //Создание колоды
+    private void makeDeck() {
         int i = 0;
         Rank[] rankArray = Rank.values();
         for (Suit suit : Suit.values()) {
@@ -24,6 +29,15 @@ public class Deck {
         }
     }
 
+    //Перетосовка
+    public void reshuffle() {
+        for (int card = 0; card < deck.length; card++) {
+            int newPosition = random.nextInt(deck.length);
+            Card timeCard = deck[card];
+            deck[card] = deck[newPosition];
+            deck[newPosition] = timeCard;
+        }
+    }
 
     @Override
     public String toString() {
--- src/src/CardPAc/Main.java
+++ src/src/CardPAc/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck(36);
         try {
-            deck.makeDeck();
+            //Создание колоды
+            System.out.println(deck);
+
+            //Перетасовка
+            deck.reshuffle();
+            System.out.println(deck);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(deck);
+
     }
 }
